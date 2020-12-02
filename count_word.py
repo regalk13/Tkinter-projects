8e268a168a298a1626d258dc89f6acb46ee069d2
Sebastian Andrade <sebasandroide05@gmail.com>
2020-12-02 14:16:35 -0500

    I added a root.geometry to establish a default window size, also i used a class to be more tidy with the GUI

--- count_word.py
+++ count_word.py
@@ -1,13 +1,9 @@
+#Imports
 import tkinter as tk
 from tkinter import *
 from collections import Counter
-root = tk.Tk()
-root.title("Count words")
 
-e2 = tk.StringVar()
-e = tk.Entry(root, textvariable=e2, width=35, borderwidth=5)
-e.pack()
-
+#Functions
 def countWords(s):
     signos = [',', '.', ';', ':']
     cleanstr = ''
@@ -17,18 +13,42 @@ def countWords(s):
         else:
             cleanstr += letra
     strlist = cleanstr.split(' ')
-    return Counter(strlist)
+    return dict(Counter(strlist))
+
 
 def button_count():
-    text = e2.get()
+    text = mainWindow.e2.get()
 
     count = countWords(text)
     myLabel = Label(root, text=count)
     myLabel.pack()
 
-button = Button(root, text="Count words", command=button_count)
-button.pack()
-exit_button = Button(root, text="Exit", command=root.quit).pack()
 
+#Graphics
+root = tk.Tk()
+root.title("Count words")
+root.geometry('400x400')
+
+#Class Window
+class Window:
+
+    def __init__(self, root):
+        self.root = root
+        self.e2 = tk.StringVar()
+        self.e = tk.Entry(root, textvariable=self.e2, width=35, borderwidth=5)
+        self.e.pack()
+
+        self.button = Button(root, text="Count words", command=button_count)
+        self.button.pack()
+        self.exit_button = Button(root, text="Exit", command=root.quit)
+        self.exit_button.pack()
+
+
+if __name__ == '__main__':
+    
+    mainWindow = Window(root)
+
+        
+    
 
 root.mainloop()
